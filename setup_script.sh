0c326d7df312196ee76ccef40f9d210987447263
dragonwarrior <vrinda@Vrindas-MacBook-Air.local>
2024-12-21 18:33:39 -0800

    fix script

--- setup_script.sh
+++ setup_script.sh
@@ -9,12 +9,6 @@ apt install python3.12-dev
 # Install Poetry
 pip install poetry
 
-# Clone the repository
-git clone https://github.com/horus-ai-labs/DistillFlow.git
-
-# Navigate to the project directory
-cd DistillFlow
-
 # Activate Poetry shell
 #poetry shell
 
