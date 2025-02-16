bba27c7a527316b0163af74aa3e5a6d9544065d6
aayuguptml <aayuguptml@gmail.com>
2025-02-15 17:52:42 -0800

    thinkk tags added

--- src/distillflow/model/tokenizer.py
+++ src/distillflow/model/tokenizer.py
@@ -34,12 +34,12 @@ def load_tokenizer(model_args: ModelArgs, template: str = None, padding_side='ri
 
     if model_args.new_special_tokens is not None:
         print(model_args.new_special_tokens)
-        exit()
+        # exit()
         num_added_tokens = tokenizer.add_special_tokens(
-            dict(additional_special_tokens=model_args.new_special_tokens),
+            dict(additional_special_tokens=model_args.new_special_tokens.split(',')),
             replace_additional_special_tokens=False,
         )
-        logger.info("Add {} to special tokens.".format(",".join(model_args.new_special_tokens)))
+        logger.info("Add {} to special tokens.".format(model_args.new_special_tokens))
         if num_added_tokens > 0 and not model_args.resize_vocab:
             model_args.resize_vocab = True
             logger.warning("New tokens have been added, changed `resize_vocab` to True.")
